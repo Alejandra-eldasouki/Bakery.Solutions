a2f24c695899d26e97718affc41ef671101e1d30
Alejae1998 <alejagr1998@gmail.com>
2023-07-14 09:45:25 -0700

    failing test one

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
 
-namespace Bakery.Tests.ModelTests
+namespace Bakery.Tests
 {
     [TestClass]
     public class BreadTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void BreadConst_CreatesInstanceOfBread_Bread()
         {
-            Assert.IsTrue(true);
+            Bread addBread = new Bread(1,5);
+            Assert.AreEqual(typeof(Bread), addBread.GetType());
         }
     }
 }
