ac355bcacf22565d62db27175baa0fc6c15f4827
Alejae1998 <alejagr1998@gmail.com>
2023-07-15 22:29:47 -0700

    failing to compile

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using Bakery.Models;
+
+public class Program
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("------------------------------");
+        Console.WriteLine("Welcome to Pierre's Bakery!");
+        Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $5.");
+        Console.WriteLine("Pastry: Buy 3, get 1 free. A single pastry costs $2.");
+        Console.WriteLine();
+    }
+}
